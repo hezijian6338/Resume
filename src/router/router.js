6bf3da261479cd8ca911fe368d5dc3c7d61b4517
hezijian6338 <hezijian6338@gamil.com>
2018-06-19 22:00:58 +0800

    更新vue-pdf组件

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,4 @@
 import Main from '@/views/Main.vue'
-import mainFrame from '@/views/mainFrame.vue'
-import stepFrame from '@/views/userPage/selfSkills.vue'
 
 // 不作为Main组件的子页面展示的页面单独写，如下
 export const loginRouter = {
